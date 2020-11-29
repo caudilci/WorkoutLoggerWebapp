065b69b775f2983ee796cf593ba4f10af648792f
Chris Caudill <christopheriancaudill@gmail.com>
2020-11-28 21:15:01 -0800

    Implemented Sign In functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-import firebaseHelper from './models/FirebaseHelper'
+import { firebaseConfig } from './models/FirebaseConfig';
 
-firebase.initializeApp(firebaseHelper);
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -21,9 +22,30 @@ function App() {
 
   return (
     <div className="App">
-      
+      <header>
+        <title>Workout Logger</title>
+      </header>
+
+      <section>
+        {user ? <Home /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <Button variant="primary" onClick={signInWithGoogle}>Sign in With Google</Button>
+  );
+}
+
+function Home() {
+
 }
 
 export default App;
